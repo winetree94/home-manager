cf03aec9ea6c8610044dd71397aff831a6f359d1
ila-park <ila@swit.io>
2024-04-02 12:46:11 +0900

    update

--- programs/zsh/default.nix
+++ programs/zsh/default.nix
@@ -2,6 +2,10 @@
 { pkgs, ... }:
 
 {
+  home.packages = with pkgs; [
+    fzf
+  ];
+
   programs.zsh = {
     enable = true;
     autosuggestion.enable = true;
@@ -21,8 +25,22 @@
         # "docker"
         # "docker-compose"
         "sudo"
+        "zoxide"
         "zsh-interactive-cd"
+        "colored-man-pages"
+        "fancy-ctrl-z"
       ];
     };
+    # plugins = [
+    #   {
+    #     name = "zsh-syntax-highlighting";
+    #     src = pkgs.fetchFromGitHub {
+    #       owner = "zsh-users";
+    #       repo = "zsh-syntax-highlighting";
+    #       rev = "2d60a47cc407117815a1d7b331ef226aa400a344";
+    #       sha256 = "1pnxr39cayhsvggxihsfa3rqys8rr2pag3ddil01w96kw84z4id2";
+    #     };
+    #   }
+    # ];
   };
 }
