1de008a1f49232ebc39ba973712bb0bad2c66c79
winetree94 <winetree94@gmail.com>
2024-04-03 20:23:51 +0900

    update

--- programs/ssh/default.nix
+++ programs/ssh/default.nix
@@ -1,37 +1,8 @@
 { pkgs, ... }:
 
 {
-
-  home.file = {
-    "./.ssh/winetree94.key" = {
-      source = ./configs/winetree94.key;
-    };
-    "./.ssh/winetree94.pub" = {
-      source = ./configs/winetree94.pub;
-    };
-    "./.ssh/swit_ila.key" = {
-      source = ./configs/swit_ila.key;
-    };
-    "./.ssh/swit_ila.pub" = {
-      source = ./configs/swit_ila.pub;
-    };
-  };
-
   programs.ssh = {
     enable = true;
-
-    matchBlocks = {
-      winetree94 = {
-        hostname = "winetree94";
-        user = "git";
-        identityFile = "~/.ssh/winetree94.key";
-      };
-      swit_ila = {
-        hostname = "swit_ila";
-        user = "git";
-        identityFile = "~/.ssh/swit_ila.key";
-      };
-    };
   };
 }
 
